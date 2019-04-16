294a7b6dcb6347190280ccc084f495408ffbdf17
Taeho Jeon <thjeon.wa@gmail.com>
2019-04-16 13:58:38 -0700

    Implement cobra class

--- snakes.py
+++ snakes.py
@@ -8,7 +8,7 @@ class Cobra(Snake):
     
     def bite(self, other):
         """Deliver a dose of venom."""
-        pass
+        print("Snake~~~~~~~~~~~~~", other)
 
     
 class BoaConstrictor(Snake):
